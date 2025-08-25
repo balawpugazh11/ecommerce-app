dd3560c49d4037b69c361d19a387d3a261b24f7b
balawpugazh11 <balawpugazh@gmail.com>
2025-08-25 18:10:47 +0530

    Fix Firebase API key

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,47 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Profile from './pages/profile';
-import Checkout from './components/Checkout';
-import About from './components/About';
-import Review from './pages/Review';
-import { CartProvider } from './context/CartContext'
+  import { BrowserRouter, Routes, Route } from 'react-router-dom';
+  import Navbar from './components/Navbar';
+  import Footer from './components/Footer';
+  import Home from './pages/Home';
+  import Products from './pages/Products';
+  import ProductDetail from './pages/ProductDetail';
+  import Cart from './pages/Cart';
+  import Login from './pages/Login';
+  import Register from './pages/Register';
+  import Profile from './pages/profile';
+  import Checkout from './components/Checkout';
+  import About from './components/About';
+  import Review from './pages/Review';
+  import { CartProvider } from './context/CartContext'
 
 
-function App() {
-  return (
-    <BrowserRouter>
-      <CartProvider>
-        <div className="min-h-screen bg-black flex flex-col">
-          <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/products/:id" element={<ProductDetail />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/checkout" element={<Checkout />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/Review" element={<Review />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </CartProvider>
-    </BrowserRouter>
-    
-  );
-}
+
+  function App() {
+    return (
+      <BrowserRouter>
+        <CartProvider>
+          <div className="min-h-screen bg-black flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/products/:id" element={<ProductDetail />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/Review" element={<Review />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </CartProvider>
+      </BrowserRouter>
+      
+    );
+  }
 
 
-export default App;
+  export default App;
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,24 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase configuration via environment variables
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBjEDtwlciL9o9dk26suMr_2bgASiVC0gs",
+  authDomain: "ecommerce-balaw.firebaseapp.com",
+  projectId: "ecommerce-balaw",
+  storageBucket: "ecommerce-balaw.firebasestorage.app",
+  messagingSenderId: "319340991757",
+  appId: "1:319340991757:web:c5303ebe0be5c84ddd4c44",
+  measurementId: "G-BFJDE72SGM"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-let analytics;
+let analytics = getAnalytics(app);
+
 try {
   analytics = getAnalytics(app);
 } catch (_) {
